0b00b664539982227b1ff30d95573bd6a7d2a1f2
Shreyansh Singh <singhshreyansh288@gmail.com>
2025-09-01 23:29:12 +0530

    hopefully this fixes it all

--- api/test.js
+++ api/test.js
@@ -1,5 +1,5 @@
 module.exports = async function handler(req, res) {
-    res.json({ 
+    res.json({
         message: 'Simple test endpoint working!',
         method: req.method,
         timestamp: new Date().toISOString()
